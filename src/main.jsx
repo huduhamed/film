0f21780d03f979fc94342f49877eaad29366eb83
hudu <ahmedhudu21@gmail.com>
2025-12-23 19:31:15 +0100

    update packages

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,3 @@
-// external imports for react native and expo files
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 
 export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, process.cwd());
+	const env = loadEnv(mode, process.cwd(), '');
 
 	return {
-		define: {
-			'import.meta.env': env,
+		plugins: [react(), env.VITE_ESLINT_NO_DEV_ERRORS === 'true' ? null : eslint()].filter(Boolean),
+
+		resolve: {
+			dedupe: ['react', 'react-dom'],
 		},
-		plugins: [env.VITE_ESLINT_NO_DEV_ERRORS === 'true' ? null : eslint()].filter(Boolean),
 	};
 });
